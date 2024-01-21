c5e898d050335991c56b628e7f8b5bff4e38fff7
Paulo André Cardoso Silva Santos <pauloandrecss@gmail.com>
2024-01-21 16:34:15 -0300

    psw

---
+++ usuarios/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas devem ser correspondentes')
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuario ja existente')
+            return redirect('/usuarios/cadastro')
+            
+        try:
+            User.objects.create_user(
+                username=username,
+                password=senha,
+            )
+            return redirect('/usuarios/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sevidor')
+            return redirect('/usuarios/cadastro')
+
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+    user = auth.authenticate(request, username=username, password=senha)
+    
+    if user:
+        auth.login(request, user)
+        messages.add_message(request, constants.SUCCESS, 'Logado!')
+        return redirect('/flashcard/novo_flashcard')
+    else:
+        messages.add_message(request, constants.ERROR, 'Usuario ou senha invalidos!')
+        return redirect('/usuarios/logar')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/usuarios/logar')
